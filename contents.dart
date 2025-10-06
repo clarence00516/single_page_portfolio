a4d5b922920c86116eb40b7cdb35840c760a2827
clarence00516 <clarence00516@gmail.com>
2025-10-06 13:35:02 +0800

    contents.dart

--- contents.dart
+++ contents.dart
@@ -7,72 +7,80 @@ class Contents extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
 
     return ListView(
       padding: const EdgeInsets.all(10.0),
       children: [
-        // Circle Picture
-        Center(
-          child: Container(
-            margin: const EdgeInsets.symmetric(vertical: 10.0),
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
-              boxShadow: [
-                BoxShadow(
-                  color: const Color.fromARGB(106, 0, 0, 0),
-                  blurRadius: 8,
-                  spreadRadius: 2,
-                  offset: const Offset(2, 4),
-                )
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(100),
-              child: Image.asset(
-                "assets/my_image.png",
-                fit: BoxFit.cover,
+
+  Card(
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(15),
+  ),
+
+  elevation: 4,
+  margin: const EdgeInsets.all(10),
+  child: Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+
+      children: [
+        // Image on the left
+        Container(
+          height: 150,
+          width: 140,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                blurRadius: 6,
+                offset: const Offset(2, 4),
               ),
+            ],
+          ),
+
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.asset(
+              "assets/my_image.png",
+              fit: BoxFit.cover,
             ),
           ),
         ),
 
-        // Student Details
-        Container(
-          height: 300,
-          margin: const EdgeInsets.symmetric(vertical: 10.0),
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: isDarkMode
-                ? const Color.fromARGB(255, 0, 0, 0)
-                : const Color.fromARGB(255, 218, 217, 217),
-            borderRadius: BorderRadius.circular(10),
-          ),
+        const SizedBox(width: 16),
+
+        Expanded(
+          flex: 2,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: const [
               Text(
                 "STUDENT DETAILS",
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
               ),
+              SizedBox(height: 8),
               Text(
-                "Full name: Clarence M. Cajardo",
+                "\nFull name: Clarence M. Cajardo",
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.w200),
               ),
               Text(
-                "Year and Section: BSIT-3A",
+                "\nYear and Section: BSIT-3A",
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.w200),
               ),
+              SizedBox(height: 8),
               Text(
-                "Personal Quote: \n\n'I am a student of University of Science and Technology of Southern Philippines, although struggles and suffering are inevitable, I will keep moving towards my goal.'",
+                "\nPersonal Quote:\n\n'I am a student of University of Science and Technology of Southern Philippines, although struggles and suffering are inevitable, I will keep moving towards my goal.'",
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.w200),
               ),
             ],
           ),
         ),
+      ],
+    ),
+  ),
+),
 
         
         // Skills Section
