0c7743a216a81cf4719a4a3f60f397c4016f60cd
clarence00516 <clarence00516@gmail.com>
2025-10-06 12:52:46 +0800

    contents.dart

--- contents.dart
+++ contents.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'projects_section.dart';
+import 'skills_section.dart';
 
 class Contents extends StatelessWidget {
   const Contents({super.key});
@@ -10,7 +12,7 @@ class Contents extends StatelessWidget {
     return ListView(
       padding: const EdgeInsets.all(10.0),
       children: [
-        // PROFILE PICTURE
+        // Circle Picture
         Center(
           child: Container(
             margin: const EdgeInsets.symmetric(vertical: 10.0),
@@ -37,7 +39,7 @@ class Contents extends StatelessWidget {
           ),
         ),
 
-        // STUDENT DETAILS
+        // Student Details
         Container(
           height: 300,
           margin: const EdgeInsets.symmetric(vertical: 10.0),
@@ -65,121 +67,22 @@ class Contents extends StatelessWidget {
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.w200),
               ),
               Text(
-                "\nPersonal Quote: \n\n'I am a student of University of Science and Technology of Southern Philippines, although struggles and suffering are inevitable, I will keep moving towards my goal.'",
+                "Personal Quote: \n\n'I am a student of University of Science and Technology of Southern Philippines, although struggles and suffering are inevitable, I will keep moving towards my goal.'",
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.w200),
               ),
             ],
           ),
         ),
 
-
-
-        // PROJECTS
-        Container(
-
-  height: 350,
-  margin: const EdgeInsets.symmetric(vertical: 20.0),
-  child: Column(
-    
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      const Text(
-        "PROJECTS",
-        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-      ),
-      const SizedBox(height: 40),
-
-      // First row (2 projects)
-      Row(
         
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // Skills Section
+        const SkillsSection(),
+
+
+        // Projects Section
+        const ProjectsSection(),
+
         
-        children: [
-          
-          Column(
-            children: [
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: Image.asset("assets/my_image.png", fit: BoxFit.cover),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: const Text("Project 1"),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: Image.asset("assets/my_image.png", fit: BoxFit.cover),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: const Text("Project 2"),
-              ),
-            ],
-          ),
-        ],
-      ),
-
-      const SizedBox(height: 20),
-
-      // Second row (2 projects)
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Column(
-            children: [
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: Image.asset("assets/my_image.png", fit: BoxFit.cover),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: const Text("Project 3"),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: Image.asset("assets/my_image.png", fit: BoxFit.cover),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: const Text("Project 4"),
-              ),
-            ],
-          ),
-        ],
-      ),
-    ],
-  ),
-),
-
-
-        // SKILLS
-        Container(
-          height: 200,
-          margin: const EdgeInsets.symmetric(vertical: 10.0),
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: isDarkMode
-                ? const Color.fromARGB(255, 0, 0, 0)
-                : const Color.fromARGB(255, 218, 217, 217),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: const Text(
-            "SKILLS",
-            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-          ),
-        ),
       ],
     );
   }
